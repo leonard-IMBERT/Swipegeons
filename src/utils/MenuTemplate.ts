7a73391f0d882032b2aa42b15a8b6c1d089ed669
Leonard Imbert <imbert418@gmail.com>
2021-01-05 16:19:26 +0100

    [Doc] Document some code

--- src/utils/MenuTemplate.ts
+++ src/utils/MenuTemplate.ts
@@ -1,12 +1,24 @@
 import { GameObjects, Scene } from "phaser";
 import Swipegeons from "../Swipegeons";
 
+/**
+* The different action that can be done on entries
+**/
 export enum Actions {
   Enter,
   Left,
   Right,
 }
 
+/**
+* @typedef {Object} Entry             - A menu entry
+* @param T                            - Type of the value stored in the entry
+* @property {string} name             - The name to display for the entry
+* @property {function} action         - Callback when an action made on the entry
+* @property {function} visualizer     - Transform a value into a displayable string
+* @property {function} getValue       - Get the value of the entry
+* @property {GameObjects.Text} repr   - The ingame object representing the entry
+**/
 type Entry<T> = {
   name: string;
   action: (act: Actions) => void;
@@ -16,23 +28,60 @@ type Entry<T> = {
 }
 
 export default class MenuTemplate {
+  /**
+  * Array containing the entries of this menu
+  * @type {Entry<number | string>[]}
+  **/
   private entries: Entry<number | string>[];
 
+  /**
+  * The scene the menu belong to
+  **/
   private scene: Scene;
 
+  /**
+  * The step (in px) between two entry)
+  **/
   private step: number;
 
+  /**
+  * The index of the currently selected entry
+  **/
   private selection = 0;
 
-  private selector: GameObjects.Text;
+  /**
+  * The game object representing the selector
+  **/
+  private selector?: GameObjects.Text;
 
+  /**
+  * The padding (in px) with the top border
+  **/
   private paddingTop = 50;
 
+  /**
+  * The padding (in px) with the left border
+  **/
   private paddingLeft = 200;
 
+  /**
+  * The font size used to display text
+  **/
   private fontSize = 34;
+
+  /**
+  * The color of the text
+  **/
   private color = "#fff";
+
+  /**
+  * The alignement of the text
+  **/
   private align = "center";
+
+  /**
+  * The font family to use
+  **/
   private family = "Mosco_Mule";
 
   private textOpt = () => { return {
@@ -54,8 +103,15 @@ export default class MenuTemplate {
     if(this.scene.game instanceof Swipegeons && this.scene.game.isMobile) this.fontSize = 54;
     if (paddingTop) this.paddingTop = paddingTop;
     if (paddingLeft) this.paddingLeft = paddingLeft;
+    this.step = this.fontSize + 2;
   }
 
+  /**
+  * Add an entry to the menu
+  * @param {Entry<number | string>} entry - The entry to add
+  * @param {number?} pos                  - The position of the new entry (default: the end of the list)
+  * @returns {MenuTemplate} this
+  **/
   public addEntry(entry: Entry<number | string>, pos?: number): MenuTemplate {
     if (pos) {
       const beg = this.entries.slice(0, pos - 1);
@@ -68,6 +124,10 @@ export default class MenuTemplate {
     return this;
   }
 
+
+  /**
+  * Reset the selector to the 0 position
+  **/
   public reInitSelection(): void {
     this.selection = 0;
     if (this.selector) {
@@ -77,6 +137,11 @@ export default class MenuTemplate {
     }
   }
 
+  /**
+  * Remove an entry
+  * @param {string | number} name - The name of the entry or its index
+  * @returns {MenuTemplate} this
+  **/
   public removeEntry(name: string | number): MenuTemplate {
     if (typeof name === 'number') {
       this.entries.splice(name, 1);
@@ -91,14 +156,17 @@ export default class MenuTemplate {
     return (select - Math.ceil(this.entries.length/2)) * this.step + middle;
   }
 
+  /**
+  * Draw the menu on the screen
+  * @return {MenuTemplate} this
+  **/
   public draw(): MenuTemplate {
-    this.step = this.fontSize + 2;
     // I know what I'm doing, don't worry
     /* eslint-disable no-param-reassign */
     this.entries.forEach((entry: Entry<number | string>, index: number) => {
       if (entry.repr == null) entry.repr = this.scene.add.text(0, 0, '', this.textOpt());
       entry.repr.setPosition(this.paddingLeft, this.getPosition(index));
-      if (entry.visualizer != null) {
+      if (entry.visualizer != null && entry.getValue != null) {
         entry.repr.setText(`${entry.name}: ${entry.visualizer(entry.getValue())}`);
       } else {
         entry.repr.setText(`${entry.name}`);
@@ -116,24 +184,36 @@ export default class MenuTemplate {
     return this;
   }
 
+  /**
+  * Go one item up
+  * @returns {MenuTemplate} this
+  **/
   public keyUp(): MenuTemplate {
     if (this.selection > 0) {
       this.selection -= 1;
-      this.selector.setY(this.getPosition(this.selection));
+      this.selector?.setY(this.getPosition(this.selection));
     }
 
     return this;
   }
 
+  /**
+  * Go one item down
+  * @returns {MenuTemplate} this
+  **/
   public keyDown(): MenuTemplate {
     if (this.selection < this.entries.length - 1) {
       this.selection += 1;
-      this.selector.setY(this.getPosition(this.selection));
+      this.selector?.setY(this.getPosition(this.selection));
     }
 
     return this;
   }
 
+  /**
+  * Select an item (if possible) that is at those coordinate
+  * @returns {MenuTemplate} this
+  **/
   public select(x: number, y: number): MenuTemplate {
     const selection = this.entries.reduceRight((select, entrie, index) => {
       if(entrie.repr == null) return select;
@@ -148,17 +228,24 @@ export default class MenuTemplate {
     return this;
   }
 
+  /**
+  * Send an actions to the menu
+  * @param {Actions} act - The action to execute
+  * @returns {MenuTemplate} this
+  **/
   public keyPressed(act: Actions): MenuTemplate {
-    this.entries[this.selection].action(act);
-    if (this.entries[this.selection].repr != null
-      && this.entries[this.selection].visualizer != null) {
-      this.entries[this.selection].repr.setText(
-        `${this.entries[this.selection].name}: ${this.entries[this.selection].visualizer(this.entries[this.selection].getValue())}`,
-      );
+    const selection = this.entries[this.selection];
+    selection.action(act);
+    if (selection.repr != null && selection.visualizer != null && selection.getValue != null) {
+      selection.repr.setText(`${selection.name}: ${selection.visualizer(selection.getValue())}`);
     }
     return this;
   }
 
+  /**
+  * Hide the menu
+  * @returns {MenuTemplate} this
+  **/
   public hide(): MenuTemplate {
     // I know what I'm doing, don't worry
     /* eslint-disable no-param-reassign */
@@ -167,12 +254,15 @@ export default class MenuTemplate {
     });
     /* eslint-enable no-param-reassign */
 
-    this.selector.visible = false;
+    if(this.selector) this.selector.visible = false;
 
     return this;
   }
 
-
+  /**
+  * Show the menu
+  * @returns {MenuTemplate} this
+  **/
   public show(): MenuTemplate {
     // I know what I'm doing, don't worry
     /* eslint-disable no-param-reassign */
@@ -181,7 +271,7 @@ export default class MenuTemplate {
     });
     /* eslint-enable no-param-reassign */
 
-    this.selector.visible = true;
+    if(this.selector) this.selector.visible = true;
 
     return this;
   }
